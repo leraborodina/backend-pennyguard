ba37a7ff2c3793f213e840dcb8ec3b4a897191c9
leraborodina <valeraborodina29@gmail.com>
2024-04-27 14:37:25 +0200

    add category service

--- src/main/java/ru/itcolleg/transaction/service/CategoryService.java
+++ src/main/java/ru/itcolleg/transaction/service/CategoryService.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface CategoryService {
+
     Optional<Category> save(Category category);
 
     Optional<Category> getById(Long id);
